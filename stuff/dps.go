2d4f6cf3b66973bdef6a644e93c859ae74c2bb16
TheBoringDude <iamcoderx@gmail.com>
2021-09-08 03:02:50 +0800

    implement new api changes

--- stuff/dps.go
+++ stuff/dps.go
@@ -9,36 +9,42 @@ import (
 	"github.com/World-of-Cryptopups/cordy/utils"
 )
 
+type FetchDPSResponse struct {
+	Success bool            `json:"success"`
+	Data    lib.UserDPSInfo `json:"data"`
+	Message string          `json:"message"`
+}
+
 // FetchDPS is a fetcher to call the endpoint and save to db.
 func FetchDPS(user lib.UserDPSUser, wallet string) (lib.UserDPSInfo, error) {
-	r, err := utils.PostFetcher(user, os.Getenv("DPS_FETCH")+wallet)
+	r, err := utils.PostFetcher(user, fmt.Sprintf("%s/dps/calculator/%s", os.Getenv("CORDY_API"), wallet))
 	if err != nil {
 		fmt.Printf("\n error in posting data in FetchDPS, error: %v", err)
 		return lib.UserDPSInfo{}, err
 	}
 
-	var data lib.UserDPSInfo
+	var data FetchDPSResponse
 	if err := json.Unmarshal(r, &data); err != nil {
 		fmt.Printf("\n error in unmarshalling data in FetchDPS, error: %v", err)
 		return lib.UserDPSInfo{}, err
 	}
 
-	return data, nil
+	return data.Data, nil
 }
 
 // Get the DPS of a certain discordId user.
 func GetDPS(id string) (lib.UserDPSInfo, error) {
-	r, err := utils.Fetcher(os.Getenv("DPS_GET") + id)
+	r, err := utils.Fetcher(fmt.Sprintf("%s/dps/%s", os.Getenv("CORDY_API"), id))
 	if err != nil {
 		fmt.Printf("\n error in posting data in GetDPS, error: %v", err)
 		return lib.UserDPSInfo{}, err
 	}
 
-	var data lib.UserDPSInfo
+	var data FetchDPSResponse
 	if err := json.Unmarshal(r, &data); err != nil {
 		fmt.Printf("\n error in unmarshalling data in GetDPS, error: %v", err)
 		return lib.UserDPSInfo{}, err
 	}
 
-	return data, nil
+	return data.Data, nil
 }
