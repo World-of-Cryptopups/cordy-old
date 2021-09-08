005f78f2a90d67dbad6f69c486d8f238f8d15f21
TheBoringDude <iamcoderx@gmail.com>
2021-09-08 10:45:30 +0800

    fix changed function in getting current pass

--- stuff/seasonpass.go
+++ stuff/seasonpass.go
@@ -2,7 +2,6 @@ package stuff
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 
 	"github.com/World-of-Cryptopups/cordy/lib"
@@ -11,7 +10,7 @@ import (
 
 // GetCurrentPass gets the current user's season pass.
 func GetCurrentPass(wallet string) (lib.SeasonPassVerify, error) {
-	r, err := utils.Fetcher(fmt.Sprintf("%s/seasonpass/one/%s", os.Getenv("CORDY_API"), wallet))
+	r, err := utils.Fetcher(os.Getenv("SEASONPASS_CURRENT_GET") + wallet)
 	if err != nil {
 		return lib.SeasonPassVerify{}, err
 	}
