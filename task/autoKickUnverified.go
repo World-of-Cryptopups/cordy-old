21fc6dcb85df93011847b6a7f6d7471dfc0eac40
TheBoringDude <iamcoderx@gmail.com>
2021-09-12 23:22:33 +0800

    final test with new checkers

--- task/autoKickUnverified.go
+++ task/autoKickUnverified.go
@@ -11,6 +11,51 @@ import (
 	"github.com/diamondburned/arikawa/v2/discord"
 )
 
+func handleKick(c *bot.Context, members []discord.Member, guildID discord.GuildID, AdventureRole discord.RoleID) error {
+	fmt.Printf("total members: %d\n", len(members))
+	for i, v := range members {
+		var hasAdventureRole bool = false
+
+		// pass if bot
+		if v.User.Bot {
+			continue
+		}
+
+		for _, x := range v.RoleIDs {
+			if AdventureRole == x {
+				hasAdventureRole = true
+				break
+			}
+		}
+
+		if !hasAdventureRole {
+			// if joined within the last 3 days, wait to verify
+			if time.Since(v.Joined.Time()).Hours() < 72 {
+				continue
+			}
+
+			fmt.Printf("-> %s | hasAdventureRole =>  %t\n", v.User.Tag(), hasAdventureRole)
+		}
+
+		if i == len(members)-1 {
+			if i == 999 {
+				m, err := c.MembersAfter(guildID, v.User.ID, 1000)
+				if err != nil {
+					fmt.Println(err)
+					break
+				}
+
+				return handleKick(c, m, guildID, AdventureRole)
+			}
+		}
+		// if !hasAdventureRole {
+		// 	c.KickWithReason(GuildID, v.User.ID, "Unverified User")
+		// }
+	}
+
+	return nil
+}
+
 func AutoKickUnverified(c *bot.Context) {
 	// this is meant for auto unverified members management
 	for {
@@ -23,30 +68,8 @@ func AutoKickUnverified(c *bot.Context) {
 			break
 		}
 
-		for _, v := range members {
-			var hasAdventureRole bool = true
+		handleKick(c, members, GuildID, discord.RoleID(AdventureRole))
 
-			// pass if bot
-			if v.User.Bot {
-				continue
-			}
-
-			for _, x := range v.RoleIDs {
-				if x == discord.RoleID(AdventureRole) {
-					hasAdventureRole = true
-					break
-				}
-			}
-
-			if !hasAdventureRole {
-				fmt.Printf("-> %s | hasAdventureRole =>  %t\n", v.User.Tag(), hasAdventureRole)
-
-			}
-			// if !hasAdventureRole {
-			// 	c.KickWithReason(GuildID, v.User.ID, "Unverified User")
-			// }
-		}
-
-		time.Sleep(time.Duration(5) * time.Minute)
+		time.Sleep(time.Duration(1) * time.Hour)
 	}
 }
